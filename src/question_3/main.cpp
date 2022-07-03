a370374c3654874eff0dfe89f5de7381106f9f67
alexhickam <alex.hickam@g.austincc.edu>
2022-07-02 19:14:03 -0500

    removed past comment

--- src/question_3/main.cpp
+++ src/question_3/main.cpp
@@ -18,17 +18,17 @@ int main()
 
         cin>>widgets_sold;
         while(1)
-{
+        {
             if(cin.fail())
             {
                 cin.clear();
                 cin.ignore(numeric_limits<streamsize>::max(),(char)'\n');
-                cout<<"You have entered an invalid input."<<"\n"<<"Please enter a whole number or interger value for widgits sold."<< "\n";
+                cout<<"You have entered an invalid input."<<"\n"<<"Please enter an interger or numerical value for widgits sold."<< "\n";
                 cin>>widgets_sold;
             }
                 if(!cin.fail())
                 break;
-            }
+        }
 
         cin.get();
 
@@ -42,5 +42,3 @@ int main()
 
     return 0;
 }
-
-//why won't this run? it runs when I move it to question 4 compiler
